5d87f2c07d4dfe31d18a18904f383590d3273f2f
Ryan Crichton <rg.crichton@gmail.com>
2012-06-18 14:38:50 +0000

    Integrated TS with RHEA HIM (interoperability layer)

---
+++ src/main/java/org/jembi/rhea/orchestration/RHEAORU_R01TerminologyValidator.java
@@ -0,0 +1,55 @@
+package org.jembi.rhea.orchestration;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.jembi.rhea.RestfulHttpRequest;
+import org.mule.api.MuleContext;
+import org.mule.api.MuleEventContext;
+import org.mule.api.MuleMessage;
+import org.mule.api.lifecycle.Callable;
+import org.mule.module.client.MuleClient;
+
+import ca.uhn.hl7v2.model.v25.group.ORU_R01_OBSERVATION;
+import ca.uhn.hl7v2.model.v25.group.ORU_R01_ORDER_OBSERVATION;
+import ca.uhn.hl7v2.model.v25.message.ORU_R01;
+import ca.uhn.hl7v2.parser.GenericParser;
+import ca.uhn.hl7v2.parser.Parser;
+
+public class RHEAORU_R01TerminologyValidator implements Callable {
+	
+	@Override
+	public Object onCall(MuleEventContext eventContext) throws Exception {
+		MuleContext muleContext = eventContext.getMuleContext();
+		MuleClient client = new MuleClient(muleContext);
+		MuleMessage msg = eventContext.getMessage();
+		RestfulHttpRequest payload = (RestfulHttpRequest) msg.getPayload();
+		String oru_r01_str = payload.getBody();
+		
+		Parser p = new GenericParser();
+		ORU_R01 oru_r01 = (ORU_R01) p.parse(oru_r01_str);
+		
+		ORU_R01_ORDER_OBSERVATION observations = oru_r01.getPATIENT_RESULT().getORDER_OBSERVATION();
+		
+		for (int i = 0 ; i < observations.getOBSERVATIONReps() ; i++) {
+			ORU_R01_OBSERVATION obs = observations.getOBSERVATION(i);
+			String id = obs.getOBX().getObservationIdentifier().getIdentifier().getValue();
+			String namespace = obs.getOBX().getObservationIdentifier().getCe3_NameOfCodingSystem().getValue();
+			
+			Map<String, String> idMap = new HashMap<String, String>();
+			idMap.put("id", id);
+			idMap.put("namespace", namespace);
+			
+			MuleMessage responce = client.send("vm://validateterm", idMap, null);
+			
+			String success = responce.getInboundProperty("success");
+			
+			if (!success.equals("true")) {
+				throw new Exception("Unknown term used in ORU_R01, please use only RHEA codes");
+			}
+		}
+		
+		return msg;
+	}
+	
+}
