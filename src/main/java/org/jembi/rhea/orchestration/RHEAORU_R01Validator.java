0433d20f426156dfc7a14c714db2ebd5cc9fa01f
Ryan Crichton <rg.crichton@gmail.com>
2012-06-20 08:52:02 +0000

    Integrated resource map into the save encounter orchestration workflow

--- src/main/java/org/jembi/rhea/orchestration/RHEAORU_R01Validator.java
+++ src/main/java/org/jembi/rhea/orchestration/RHEAORU_R01Validator.java
@@ -168,7 +168,7 @@ public class RHEAORU_R01Validator implements Callable {
 		// Validate location ID is correct - sending facility
 		String elid = oru_r01.getMSH().getSendingFacility().getHd1_NamespaceID().getValue();
 		
-		responce = client.send("vm://validateFacility", elid, null, 5000);
+		responce = client.send("vm://validateFacility-resourcemap", elid, null, 5000);
 		
 		success = responce.getInboundProperty("success");
 		if (!success.equals("true")) {
