5506c717a2bdfdcc66893ff7de416cc2c82cca68
Ryan Crichton <rg.crichton@gmail.com>
2012-07-12 14:45:28 +0000

    Integrated query for patients transaction with OpenEMPI

--- src/main/java/org/jembi/rhea/transformers/QueryPatientsOpenEMPIOffRampTransformer.java
+++ src/main/java/org/jembi/rhea/transformers/QueryPatientsOpenEMPIOffRampTransformer.java
@@ -1,12 +1,14 @@
 package org.jembi.rhea.transformers;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Map;
 
 import org.jembi.rhea.RestfulHttpRequest;
-import org.mule.DefaultMuleMessage;
+import org.jembi.rhea.Util;
 import org.mule.api.MuleMessage;
 import org.mule.api.transformer.TransformerException;
-import org.mule.api.transport.PropertyScope;
 import org.mule.transformer.AbstractMessageTransformer;
 
 public class QueryPatientsOpenEMPIOffRampTransformer extends AbstractMessageTransformer {
@@ -18,10 +20,54 @@ public class QueryPatientsOpenEMPIOffRampTransformer extends AbstractMessageTran
 		RestfulHttpRequest request = (RestfulHttpRequest) msg.getPayload();
 		Map<String, String> requestParams = request.getRequestParams();
 		
-		String body = "";
+		String body = "<?xml version='1.0' encoding='UTF-8'?>\n";
 		
-		body = "\n";
-		//TODO
+		body += "<person>\n";
+		
+		//dateofbirth
+		String dob_str = requestParams.get("dob");
+		if (dob_str != null) {
+			SimpleDateFormat sdf_input = new SimpleDateFormat("yyyy-MM-dd");
+			SimpleDateFormat sdf_output = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
+			try {
+				Date dob = sdf_input.parse(dob_str);
+				body += "<dateOfBirth>" + sdf_output.format(dob) + "</dateOfBirth>\n";
+				
+			} catch (ParseException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		
+		//familyname
+		String familyName = requestParams.get("family_name");
+		if (familyName != null) {
+			body += "<familyName>" + familyName + "</familyName>\n";
+		}
+		
+		//givenname
+		String givenName = requestParams.get("given_name");
+		if (givenName != null) {
+			body += "<givenName>" + givenName + "</givenName>\n";
+		}
+		
+		//gender - does this work in OpenEMPI??
+		//String gender = requestParams.get("gender");
+		
+		//identifiers
+		String id_str = requestParams.get("id");
+		if (id_str != null) {
+			String[] splitIdentifer = Util.splitIdentifer(id_str);
+			body += "<personIdentifiers>\n";
+			body += "	<identifier>" + splitIdentifer[1] + "</identifier>\n";
+			body += "	<identifierDomain>\n";
+			body += "		<universalIdentifier>" + splitIdentifer[0] + "</universalIdentifier>\n";
+			body += "		<universalIdentifierTypeCode>" + splitIdentifer[0] + "</universalIdentifierTypeCode>\n";
+			body += "	</identifierDomain>\n";
+			body += "</personIdentifiers>\n";
+		}
+		
+		body += "</person>\n";
 		
 		request.setBody(body);
 		
