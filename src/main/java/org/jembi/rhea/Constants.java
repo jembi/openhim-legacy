bbc0585653f33f473c659484ba39475afcaf9a7a
Ryan Crichton <rg.crichton@gmail.com>
2012-07-30 09:13:54 +0000

    Added Inject NID fuctioanlity to Get encounters transaction to allow the EPID to be replaced by provider NID - a single test provider's data is hard coded to make this work for now as the SHR does not support this properly yet, this  should be removed when it does.

--- src/main/java/org/jembi/rhea/Constants.java
+++ src/main/java/org/jembi/rhea/Constants.java
@@ -4,5 +4,6 @@ public class Constants {
 	
 	public static final String ECID_ID_TYPE = "ECID";
 	public static final String EPID_ID_TYPE = "EPID";
+	public static final String NID_ID_TYPE = "NID";
 	
 }
