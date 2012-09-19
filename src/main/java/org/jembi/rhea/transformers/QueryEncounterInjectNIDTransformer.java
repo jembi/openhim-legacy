6ce588caf77789cdfc65ddd558db5c400f771909
Ryan Crichton <rg.crichton@gmail.com>
2012-09-19 09:03:53 +0000

    - Changed interface component to read keystore name and password from config file for HTTPS connections - Removed erronous hard coded username and password from query encounter denorm to openmrsshr - Added message enrichment step for query encounter transaction where the ECID is replace with the NID of the person in the returned message

--- src/main/java/org/jembi/rhea/transformers/QueryEncounterInjectNIDTransformer.java
+++ src/main/java/org/jembi/rhea/transformers/QueryEncounterInjectNIDTransformer.java
@@ -3,6 +3,8 @@ package org.jembi.rhea.transformers;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.jembi.rhea.Constants;
 import org.jembi.rhea.RestfulHttpResponse;
 import org.mule.api.MuleMessage;
@@ -10,16 +12,20 @@ import org.mule.api.transformer.TransformerException;
 import org.mule.module.client.MuleClient;
 import org.mule.transformer.AbstractMessageTransformer;
 
+import ca.uhn.hl7v2.model.v25.datatype.CX;
 import ca.uhn.hl7v2.model.v25.datatype.XCN;
 import ca.uhn.hl7v2.model.v25.group.ORU_R01_PATIENT_RESULT;
 import ca.uhn.hl7v2.model.v25.message.ORU_R01;
 import ca.uhn.hl7v2.model.v25.segment.OBR;
+import ca.uhn.hl7v2.model.v25.segment.PID;
 import ca.uhn.hl7v2.parser.GenericParser;
 import ca.uhn.hl7v2.parser.Parser;
 import ca.uhn.hl7v2.validation.impl.DefaultValidation;
 
 public class QueryEncounterInjectNIDTransformer extends
 		AbstractMessageTransformer {
+	
+	Log log = LogFactory.getLog(this.getClass());
 
 	@Override
 	public Object transformMessage(MuleMessage msg, String enc)
@@ -44,6 +50,41 @@ public class QueryEncounterInjectNIDTransformer extends
 			
 			ORU_R01 oru_r01 = (ORU_R01) hl7_msg;
 			
+			// Replace ECID with Client NID
+			PID pid = oru_r01.getPATIENT_RESULT().getPATIENT().getPID();
+			CX[] patientIdentifierList = pid.getPatientIdentifierList();
+			
+			String nid = null;
+			if (patientIdentifierList.length < 1) {
+				throw new Exception("Invalid client ID");
+			}
+			
+			String id = patientIdentifierList[0].getIDNumber().getValue();
+			String idType = patientIdentifierList[0].getIdentifierTypeCode().getValue();
+			
+			Map<String, String> idMap = new HashMap<String, String>();
+			idMap.put("id", id);
+			idMap.put("idType", idType);
+			
+			MuleMessage responce = client.send("vm://getnid-openempi", idMap, null, 5000);
+			
+			String success = responce.getInboundProperty("success");
+			if (success != null && success.equals("true")) {
+				nid = responce.getPayloadAsString();
+			}
+			
+			if (nid == null) {
+				throw new Exception("Invalid Client: NID for ECID:" + id + " could not be found in Client Registry");
+			} else {
+				// Enrich the message
+				CX idCX = pid.getPatientIdentifierList(0);
+				idCX.getIdentifierTypeCode().setValue(Constants.NID_ID_TYPE);
+				idCX.getIDNumber().setValue(nid);
+				
+				log.info("Validated Client and enriched message with Client NID");
+			}
+			
+			// Validate and replace provider id's in each obr
 			for (int i = 0 ; i < oru_r01.getPATIENT_RESULTReps() ; i++) {
 				ORU_R01_PATIENT_RESULT patient_RESULT = oru_r01.getPATIENT_RESULT(i);
 				
@@ -69,10 +110,10 @@ public class QueryEncounterInjectNIDTransformer extends
 					ProIdMap.put("id", proID);
 					ProIdMap.put("idType", proIDType);
 					
-					MuleMessage responce = client.send("vm://getnid-openldap", ProIdMap, null, 5000);
+					responce = client.send("vm://getnid-openldap", ProIdMap, null, 5000);
 					
-					String nid = null;
-					String success = responce.getInboundProperty("success");
+					nid = null;
+					success = responce.getInboundProperty("success");
 					if (success.equals("true")) {
 						nid = responce.getPayloadAsString();
 						
