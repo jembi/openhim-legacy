6db067ec7a16fef6cf2d3bf914cdc158abe834b7
devcritter <venterh@gmail.com>
2012-12-04 12:59:06 +0200

    XDS refactoring. Added license headers.

---
+++ src/main/java/ca/marc/ihe/xds/XDSUtil.java
@@ -0,0 +1,150 @@
+/* 
+ * Copyright 2012 Mohawk College of Applied Arts and Technology
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you 
+ * may not use this file except in compliance with the License. You may 
+ * obtain a copy of the License at 
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0 
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the 
+ * License for the specific language governing permissions and limitations under 
+ * the License.
+ */
+package ca.marc.ihe.xds;
+
+import java.util.Arrays;
+import java.util.UUID;
+
+import oasis.names.tc.ebxml_regrep.xsd.rim._3.AssociationType1;
+import oasis.names.tc.ebxml_regrep.xsd.rim._3.ClassificationType;
+import oasis.names.tc.ebxml_regrep.xsd.rim._3.ExternalIdentifierType;
+import oasis.names.tc.ebxml_regrep.xsd.rim._3.ExtrinsicObjectType;
+import oasis.names.tc.ebxml_regrep.xsd.rim._3.InternationalStringType;
+import oasis.names.tc.ebxml_regrep.xsd.rim._3.LocalizedStringType;
+import oasis.names.tc.ebxml_regrep.xsd.rim._3.RegistryObjectType;
+import oasis.names.tc.ebxml_regrep.xsd.rim._3.RegistryPackageType;
+import oasis.names.tc.ebxml_regrep.xsd.rim._3.SlotType1;
+import oasis.names.tc.ebxml_regrep.xsd.rim._3.ValueListType;
+
+public class XDSUtil {
+
+	/**
+	 * Create registry package
+	 */
+	public static RegistryPackageType createRegistryPackage()
+	{
+		RegistryPackageType retVal = new RegistryPackageType();
+		retVal.setId(String.format("urn:uuid:%s", UUID.randomUUID().toString()));
+		return retVal;
+	}
+	
+	/**
+	 * Create Extrinsic object
+	 */
+	public static ExtrinsicObjectType createExtrinsicObject(String mimeType, String name, XdsGuidType objectType)
+	{
+
+		ExtrinsicObjectType retVal = new ExtrinsicObjectType();
+		retVal.setId(String.format("urn:uuid:%s", UUID.randomUUID().toString()));
+		retVal.setObjectType(objectType.toString());
+		retVal.setMimeType(mimeType);
+		
+		InternationalStringType localName = new InternationalStringType();
+		LocalizedStringType stringValue = new LocalizedStringType();
+		stringValue.setValue(name);
+		localName.getLocalizedString().add(stringValue);
+		retVal.setName(localName);
+		
+		return retVal;
+	}
+	
+    /**
+     * Create a query slot
+     */
+    public static SlotType1 createSlot(String slotName, String... value)
+    {
+    	SlotType1 retSlot = new SlotType1();
+        ValueListType patientValueList = new ValueListType();
+        patientValueList.getValue().addAll(Arrays.asList(value));
+        retSlot.setName(slotName);
+        retSlot.setValueList(patientValueList);
+        return retSlot;
+    }
+    
+	/**
+	 * Create association type
+	 */
+	public static AssociationType1 createAssociation(RegistryObjectType source,
+			ExtrinsicObjectType target, String status) {
+		AssociationType1 retAssoc = new AssociationType1();
+		retAssoc.setId(String.format("urn:uuid:%s", UUID.randomUUID().toString()));
+		retAssoc.setSourceObject(source.getId());
+		retAssoc.setTargetObject(target.getId());
+		retAssoc.getSlot().add(createSlot("SubmissionSetStatus", status));
+		return retAssoc;
+	}
+
+    /**
+     * Create classification object
+     */
+    public static ClassificationType createClassification(RegistryObjectType classifiedObject, XdsGuidType scheme, String nodeRepresentation, String name, SlotType1... slots)
+    {
+
+    	ClassificationType retClass = new ClassificationType();
+    	retClass.setId(String.format("urn:uuid:%s", UUID.randomUUID().toString()));
+    	retClass.setClassificationScheme(scheme.toString());
+    	retClass.setClassifiedObject(classifiedObject.getId());
+    	if (nodeRepresentation!=null) retClass.setNodeRepresentation(nodeRepresentation);
+    	retClass.getSlot().addAll(Arrays.asList(slots));
+    	
+    	if (name!=null) {
+	    	InternationalStringType localName = new InternationalStringType();
+			LocalizedStringType stringValue = new LocalizedStringType();
+			stringValue.setValue(name);
+			localName.getLocalizedString().add(stringValue);
+			retClass.setName(localName);
+    	}
+		
+    	return retClass;
+    }
+    
+    /**
+	 * Create node classification
+	 * @param registryObject
+	 * @param classificationNode
+	 * @return
+	 */
+	public static ClassificationType createNodeClassification(RegistryObjectType registryObject, XdsGuidType classificationNode)
+	{
+		ClassificationType retClass = new ClassificationType();
+		retClass.setId(String.format("urn:uuid:%s", UUID.randomUUID()));
+		retClass.setClassificationNode(classificationNode.toString());
+		retClass.setClassifiedObject(registryObject.getId());
+		return retClass;
+	}
+	
+    /**
+     * Create external identifier
+     */
+    public static ExternalIdentifierType createExternalIdentifier(RegistryObjectType registryObject, XdsGuidType scheme, String value)
+    {
+
+    	ExternalIdentifierType retId = new ExternalIdentifierType();
+    	retId.setId(String.format("urn:uuid:%s", UUID.randomUUID().toString()));
+    	
+    	retId.setRegistryObject(registryObject.getId());
+    	retId.setIdentificationScheme(scheme.toString());
+    	retId.setValue(value);
+    	
+		InternationalStringType localName = new InternationalStringType();
+		LocalizedStringType stringValue = new LocalizedStringType();
+		stringValue.setValue(scheme.getName());
+		localName.getLocalizedString().add(stringValue);
+		retId.setName(localName);
+		
+		return retId;
+    }
+}
--- src/main/java/org/jembi/rhea/transformers/XDSRepositoryProvideAndRegisterDocument.java
+++ src/main/java/org/jembi/rhea/transformers/XDSRepositoryProvideAndRegisterDocument.java
@@ -1,10 +1,12 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 package org.jembi.rhea.transformers;
 
 import ihe.iti.xds_b._2007.ProvideAndRegisterDocumentSetRequestType;
 import ihe.iti.xds_b._2007.ProvideAndRegisterDocumentSetRequestType.Document;
 
 import java.text.SimpleDateFormat;
-import java.util.Arrays;
 import java.util.Date;
 import java.util.UUID;
 
@@ -14,21 +16,17 @@ import javax.xml.namespace.QName;
 import oasis.names.tc.ebxml_regrep.xsd.lcm._3.SubmitObjectsRequest;
 import oasis.names.tc.ebxml_regrep.xsd.rim._3.AssociationType1;
 import oasis.names.tc.ebxml_regrep.xsd.rim._3.ClassificationType;
-import oasis.names.tc.ebxml_regrep.xsd.rim._3.ExternalIdentifierType;
 import oasis.names.tc.ebxml_regrep.xsd.rim._3.ExtrinsicObjectType;
-import oasis.names.tc.ebxml_regrep.xsd.rim._3.InternationalStringType;
-import oasis.names.tc.ebxml_regrep.xsd.rim._3.LocalizedStringType;
 import oasis.names.tc.ebxml_regrep.xsd.rim._3.RegistryObjectListType;
-import oasis.names.tc.ebxml_regrep.xsd.rim._3.RegistryObjectType;
 import oasis.names.tc.ebxml_regrep.xsd.rim._3.RegistryPackageType;
 import oasis.names.tc.ebxml_regrep.xsd.rim._3.SlotType1;
-import oasis.names.tc.ebxml_regrep.xsd.rim._3.ValueListType;
 
 import org.jembi.rhea.RestfulHttpRequest;
 import org.mule.api.MuleMessage;
 import org.mule.api.transformer.TransformerException;
 import org.mule.transformer.AbstractMessageTransformer;
 
+import ca.marc.ihe.xds.XDSUtil;
 import ca.marc.ihe.xds.XdsGuidType;
 import ca.uhn.hl7v2.HL7Exception;
 import ca.uhn.hl7v2.model.v25.datatype.CX;
@@ -38,10 +36,15 @@ import ca.uhn.hl7v2.model.v25.segment.PV1;
 import ca.uhn.hl7v2.parser.GenericParser;
 import ca.uhn.hl7v2.parser.Parser;
 
+/**
+ * XDS ITI-41 Provide and Register Document Set-b
+ */
 public class XDSRepositoryProvideAndRegisterDocument extends
 		AbstractMessageTransformer {
 
 	private static SimpleDateFormat formatter_yyyyMMdd = new SimpleDateFormat("yyyyMMdd");
+	
+	/* Transform ORU_R01 and generate register request */
 	
 	@Override
 	public Object transformMessage(MuleMessage src, String encoding)
@@ -56,6 +59,12 @@ public class XDSRepositoryProvideAndRegisterDocument extends
 		}
 	}
 	
+	/* 
+	 * Please note that the following method (buildRegisterRequest) contains modified code
+	 * originally written by Mohawk College and released under the Apache 2.0 license
+	 * (http://www.apache.org/licenses/LICENSE-2.0)
+	 */
+	
 	protected static ProvideAndRegisterDocumentSetRequestType buildRegisterRequest(EncounterInfo enc) {
 		ProvideAndRegisterDocumentSetRequestType xdsRequest = new ProvideAndRegisterDocumentSetRequestType();
 		SubmitObjectsRequest submissionRequest = new SubmitObjectsRequest();
@@ -65,36 +74,36 @@ public class XDSRepositoryProvideAndRegisterDocument extends
 		Date now = new Date();
 		
 		// For each document
-		ExtrinsicObjectType document = createExtrinsicObject("text/xml", "Physical", XdsGuidType.XDSDocumentEntry);
+		ExtrinsicObjectType document = XDSUtil.createExtrinsicObject("text/xml", "Physical", XdsGuidType.XDSDocumentEntry);
 		
 		// To add slots
-		document.getSlot().add(createSlot("creationTime", formatter_yyyyMMdd.format(now)));
-		document.getSlot().add(createSlot("languageCode", "en-us"));
-		document.getSlot().add(createSlot("serviceStartTime", enc.getEncounterDateTime()));
-		document.getSlot().add(createSlot("serviceStopTime", enc.getEncounterDateTime()));
-		document.getSlot().add(createSlot("sourcePatientId", enc.getPID()));
-		document.getSlot().add(createSlot("sourcePatientInfo", "PID-3|" + enc.getPID(), "PID-5|" + enc.getName()));
+		document.getSlot().add(XDSUtil.createSlot("creationTime", formatter_yyyyMMdd.format(now)));
+		document.getSlot().add(XDSUtil.createSlot("languageCode", "en-us"));
+		document.getSlot().add(XDSUtil.createSlot("serviceStartTime", enc.getEncounterDateTime()));
+		document.getSlot().add(XDSUtil.createSlot("serviceStopTime", enc.getEncounterDateTime()));
+		document.getSlot().add(XDSUtil.createSlot("sourcePatientId", enc.getPID()));
+		document.getSlot().add(XDSUtil.createSlot("sourcePatientInfo", "PID-3|" + enc.getPID(), "PID-5|" + enc.getName()));
 		
 		// To add classifications
 		SlotType1[] authorSlots = new SlotType1[] {
-			createSlot("authorPerson", enc.getAttendingDoctor()),
-			createSlot("authorInstitution", enc.getLocation()),
+			XDSUtil.createSlot("authorPerson", enc.getAttendingDoctor()),
+			XDSUtil.createSlot("authorInstitution", enc.getLocation()),
 		};
-		document.getClassification().add(createClassification(document, XdsGuidType.XDSDocumentEntry_Author, null, null, authorSlots));
+		document.getClassification().add(XDSUtil.createClassification(document, XdsGuidType.XDSDocumentEntry_Author, null, null, authorSlots));
 		
 		//TODO the class code needs to be set correctly according to the document type
 		SlotType1[] classCodeSlots = new SlotType1[] {
-			createSlot("codingScheme", "1.3.6.1.4.1.19376.1.5.3.1.1.10")
+			XDSUtil.createSlot("codingScheme", "1.3.6.1.4.1.19376.1.5.3.1.1.10")
 		};
-		document.getClassification().add(createClassification(document, XdsGuidType.XDSDocumentEntry_ClassCode, "", enc.getEncounterType(), classCodeSlots));
+		document.getClassification().add(XDSUtil.createClassification(document, XdsGuidType.XDSDocumentEntry_ClassCode, "", enc.getEncounterType(), classCodeSlots));
 		SlotType1[] confidentialitySlots = new SlotType1[] {
-			createSlot("codingScheme", "Connect-a-thon confidentialityCodes")
+			XDSUtil.createSlot("codingScheme", "Connect-a-thon confidentialityCodes")
 		};
-		document.getClassification().add(createClassification(document, XdsGuidType.XDSDocumentEntry_ConfidentialityCode, "1.3.6.1.4.1.21367.2006.7.107", "Normal", confidentialitySlots));
+		document.getClassification().add(XDSUtil.createClassification(document, XdsGuidType.XDSDocumentEntry_ConfidentialityCode, "1.3.6.1.4.1.21367.2006.7.107", "Normal", confidentialitySlots));
 		
 		// To add external ids
-		document.getExternalIdentifier().add(createExternalIdentifier(document, XdsGuidType.XDSDocumentEntry_PatientId, enc.getPID()));
-		document.getExternalIdentifier().add(createExternalIdentifier(document, XdsGuidType.XDSDocumentEntry_UniqueId, String.format("urn:uuid:%s", UUID.randomUUID())));
+		document.getExternalIdentifier().add(XDSUtil.createExternalIdentifier(document, XdsGuidType.XDSDocumentEntry_PatientId, enc.getPID()));
+		document.getExternalIdentifier().add(XDSUtil.createExternalIdentifier(document, XdsGuidType.XDSDocumentEntry_UniqueId, String.format("urn:uuid:%s", UUID.randomUUID())));
 
 		// Add to list of objects
 		registryObjects.getIdentifiable().add(new JAXBElement<ExtrinsicObjectType>(
@@ -104,16 +113,16 @@ public class XDSRepositoryProvideAndRegisterDocument extends
         ));
 
 		// Create submission set
-		RegistryPackageType pkg = createRegistryPackage();
-		pkg.getSlot().add(createSlot("submissionTime", formatter_yyyyMMdd.format(now)));
+		RegistryPackageType pkg = XDSUtil.createRegistryPackage();
+		pkg.getSlot().add(XDSUtil.createSlot("submissionTime", formatter_yyyyMMdd.format(now)));
 		// To add classifications
 		SlotType1[] contentTypeSlots = new SlotType1[] {
-				createSlot("condingScheme", "Connect-a-thon contentTypeCodes")
+				XDSUtil.createSlot("condingScheme", "Connect-a-thon contentTypeCodes")
 		};
-		pkg.getClassification().add(createClassification(document, XdsGuidType.XDSSubmissionSet_ContentType, "History and Physical", "History and Physical", contentTypeSlots));
+		pkg.getClassification().add(XDSUtil.createClassification(document, XdsGuidType.XDSSubmissionSet_ContentType, "History and Physical", "History and Physical", contentTypeSlots));
 		
 		// To add external ids
-		pkg.getExternalIdentifier().add(createExternalIdentifier(document, XdsGuidType.XDSSubmissionSet_PatientId, enc.getPID()));
+		pkg.getExternalIdentifier().add(XDSUtil.createExternalIdentifier(document, XdsGuidType.XDSSubmissionSet_PatientId, enc.getPID()));
 
 		// Add package to submission
 		registryObjects.getIdentifiable().add(new JAXBElement<RegistryPackageType>(
@@ -126,14 +135,14 @@ public class XDSRepositoryProvideAndRegisterDocument extends
 		registryObjects.getIdentifiable().add(new JAXBElement<ClassificationType>(
                 new QName("urn:oasis:names:tc:ebxml-regrep:xsd:rim:3.0", "Classification"),
                 ClassificationType.class,
-                createNodeClassification(pkg, XdsGuidType.XDSSubmissionSet)
+                XDSUtil.createNodeClassification(pkg, XdsGuidType.XDSSubmissionSet)
             ));
 		
 		// Add association
 		registryObjects.getIdentifiable().add(new JAXBElement<AssociationType1>(
                 new QName("urn:oasis:names:tc:ebxml-regrep:xsd:rim:3.0", "Association"),
                 AssociationType1.class,
-                createAssociation(pkg, document, "original")
+                XDSUtil.createAssociation(pkg, document, "original")
             ));
 		
 		// Add document
@@ -145,6 +154,14 @@ public class XDSRepositoryProvideAndRegisterDocument extends
 		return xdsRequest;
 	}
 	
+	/* */
+	
+    
+    /* Auditing */
+    /* */
+    
+    /* Parse ORU_R01 */
+    
 	protected static EncounterInfo parseEncounterRequest(String oru_r01) throws HL7Exception {
 		EncounterInfo res = new EncounterInfo();
 		Parser parser = new GenericParser();
@@ -173,124 +190,6 @@ public class XDSRepositoryProvideAndRegisterDocument extends
 		return res;
 	}
 	
-
-	/**
-	 * Create registry package
-	 */
-	private static RegistryPackageType createRegistryPackage()
-	{
-		RegistryPackageType retVal = new RegistryPackageType();
-		retVal.setId(String.format("urn:uuid:%s", UUID.randomUUID().toString()));
-		return retVal;
-	}
-	
-	/**
-	 * Create Extrinsic object
-	 */
-	private static ExtrinsicObjectType createExtrinsicObject(String mimeType, String name, XdsGuidType objectType)
-	{
-
-		ExtrinsicObjectType retVal = new ExtrinsicObjectType();
-		retVal.setId(String.format("urn:uuid:%s", UUID.randomUUID().toString()));
-		retVal.setObjectType(objectType.toString());
-		retVal.setMimeType(mimeType);
-		
-		InternationalStringType localName = new InternationalStringType();
-		LocalizedStringType stringValue = new LocalizedStringType();
-		stringValue.setValue(name);
-		localName.getLocalizedString().add(stringValue);
-		retVal.setName(localName);
-		
-		return retVal;
-	}
-	
-    /**
-     * Create a query slot
-     */
-    private static SlotType1 createSlot(String slotName, String... value)
-    {
-    	SlotType1 retSlot = new SlotType1();
-        ValueListType patientValueList = new ValueListType();
-        patientValueList.getValue().addAll(Arrays.asList(value));
-        retSlot.setName(slotName);
-        retSlot.setValueList(patientValueList);
-        return retSlot;
-    }
-    
-	/**
-	 * Create association type
-	 */
-	private static AssociationType1 createAssociation(RegistryObjectType source,
-			ExtrinsicObjectType target, String status) {
-		AssociationType1 retAssoc = new AssociationType1();
-		retAssoc.setId(String.format("urn:uuid:%s", UUID.randomUUID().toString()));
-		retAssoc.setSourceObject(source.getId());
-		retAssoc.setTargetObject(target.getId());
-		retAssoc.getSlot().add(createSlot("SubmissionSetStatus", status));
-		return retAssoc;
-	}
-
-    /**
-     * Create classification object
-     */
-    private static ClassificationType createClassification(RegistryObjectType classifiedObject, XdsGuidType scheme, String nodeRepresentation, String name, SlotType1... slots)
-    {
-
-    	ClassificationType retClass = new ClassificationType();
-    	retClass.setId(String.format("urn:uuid:%s", UUID.randomUUID().toString()));
-    	retClass.setClassificationScheme(scheme.toString());
-    	retClass.setClassifiedObject(classifiedObject.getId());
-    	if (nodeRepresentation!=null) retClass.setNodeRepresentation(nodeRepresentation);
-    	retClass.getSlot().addAll(Arrays.asList(slots));
-    	
-    	if (name!=null) {
-	    	InternationalStringType localName = new InternationalStringType();
-			LocalizedStringType stringValue = new LocalizedStringType();
-			stringValue.setValue(name);
-			localName.getLocalizedString().add(stringValue);
-			retClass.setName(localName);
-    	}
-		
-    	return retClass;
-    }
-    
-    /**
-	 * Create node classification
-	 * @param registryObject
-	 * @param classificationNode
-	 * @return
-	 */
-	private static ClassificationType createNodeClassification(RegistryObjectType registryObject, XdsGuidType classificationNode)
-	{
-		ClassificationType retClass = new ClassificationType();
-		retClass.setId(String.format("urn:uuid:%s", UUID.randomUUID()));
-		retClass.setClassificationNode(classificationNode.toString());
-		retClass.setClassifiedObject(registryObject.getId());
-		return retClass;
-	}
-	
-    /**
-     * Create external identifier
-     */
-    private static ExternalIdentifierType createExternalIdentifier(RegistryObjectType registryObject, XdsGuidType scheme, String value)
-    {
-
-    	ExternalIdentifierType retId = new ExternalIdentifierType();
-    	retId.setId(String.format("urn:uuid:%s", UUID.randomUUID().toString()));
-    	
-    	retId.setRegistryObject(registryObject.getId());
-    	retId.setIdentificationScheme(scheme.toString());
-    	retId.setValue(value);
-    	
-		InternationalStringType localName = new InternationalStringType();
-		LocalizedStringType stringValue = new LocalizedStringType();
-		stringValue.setValue(scheme.getName());
-		localName.getLocalizedString().add(stringValue);
-		retId.setName(localName);
-		
-		return retId;
-    }
-    
     
     protected static class EncounterInfo {
     	protected String pid;
@@ -325,4 +224,6 @@ public class XDSRepositoryProvideAndRegisterDocument extends
 	    	return encounterType;
 	    }
     }
+    
+    /* */
 }
