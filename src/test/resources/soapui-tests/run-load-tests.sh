27f603a1e0e8d91db9be0760a3a19e1d09d70e83
ryan <rg.crichton@gmail.com>
2013-05-02 20:49:59 +0200

    Fixed older mule server compatibility issues, updated integration test suite

--- src/test/resources/soapui-tests/run-load-tests.sh
+++ src/test/resources/soapui-tests/run-load-tests.sh
@@ -3,4 +3,4 @@ LOADUI_HOME="/home/ryan/Programs/loadUI-2.1.1/"
 
 DIR=`pwd`
 cd $LOADUI_HOME
-./loadUI-cmd.sh -p $DIR/OpenHIM-load-tests.xml -L 10:0:0 -r $DIR -F PDF -l -s -z
+./loadUI-cmd.sh -p $DIR/OpenHIM-load-tests.xml -L 600:0:0 -r $DIR -F PDF -l -s -z
