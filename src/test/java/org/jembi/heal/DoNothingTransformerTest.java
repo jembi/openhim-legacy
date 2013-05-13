f15d44cb2b87facd70c8b3504fb615c9077fc4e1
devcritter <venterh@gmail.com>
2013-05-13 13:22:48 +0200

    Refactored DoNothingTransformer unit test as per comments for #83

--- src/test/java/org/jembi/heal/DoNothingTransformerTest.java
+++ src/test/java/org/jembi/heal/DoNothingTransformerTest.java
@@ -2,38 +2,75 @@ package org.jembi.heal;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.mule.api.transformer.TransformerException;
 
+/**
+ * DoNothingTransformer must return a reference to the passed object and not modify it.
+ */
 public class DoNothingTransformerTest {
+	
+	protected DoNothingTransformer transformer;
 
+	@Before
+	public void setup() {
+		transformer = new DoNothingTransformer();
+	}
+	
 	/**
-	 * DoNothingTransformer must return a reference to the passed object and not modify it.
+	 * Test a string object
 	 */
 	@Test
-	public void testDoTransformObjectString() {
-		DoNothingTransformer transformer = new DoNothingTransformer();
-		
+	public void testDoTransformString() {
+		String test = "Test";
 		try {
-			String test = "Test";
 			Object result = transformer.doTransform(test, null);
-			assertTrue(result == test);
-			assertTrue(result.equals("Test"));
+			assertSame(result, test);
+			assertEquals(result, "Test");
+		} catch (TransformerException e) {
+			fail("Exception occured " + e.getMessage());
+		}
+	}
 			
-			//a non-string object should work
-			result = transformer.doTransform(transformer, null);
-			assertTrue(result == transformer);
+	/**
+	 * Test a non-string object
+	 */
+	@Test
+	public void testDoTransformObject() {		
+		try {
+			Object result = transformer.doTransform(transformer, null);
+			assertSame(result, transformer);
+		} catch (TransformerException e) {
+			fail("Exception occured " + e.getMessage());
+		}
+	}
 			
-			//encoding parameter should not have an effect
-			result = transformer.doTransform(test, "utf-8");
-			assertTrue(result == test);
-			assertTrue(result.equals("Test"));
+	/**
+	 * The encoding parameter should not have an effect
+	 */
+	@Test
+	public void testDoTransformEncoding() {		
+		String test = "Test";
+		try {
+			Object result = transformer.doTransform(test, "utf-8");
+			assertSame(result, test);
+			assertEquals(result, "Test");
 			result = transformer.doTransform(test, "notAValidEncodingString");
-			assertTrue(result == test);
-			assertTrue(result.equals("Test"));
+			assertSame(result, test);
+			assertEquals(result, "Test");
+		} catch (TransformerException e) {
+			fail("Exception occured " + e.getMessage());
+		}
+	}
 			
-			//null should work
-			result = transformer.doTransform(null, null);
+	/**
+	 * If null is passed as a parameter, then null should be returned
+	 */
+	@Test
+	public void testDoTransformNull() {		
+		try {
+			Object result = transformer.doTransform(null, null);
 			assertNull(result);
 		} catch (TransformerException e) {
 			fail("Exception occured " + e.getMessage());
