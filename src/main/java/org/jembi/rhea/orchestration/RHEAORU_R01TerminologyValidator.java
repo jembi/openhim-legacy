f9fb52359aebe470f0bd6351a41b309196be0f6e
devcritter <venterh@gmail.com>
2013-05-20 12:01:19 +0200

    Removed the COdeValidator interface and impl.

--- src/main/java/org/jembi/rhea/orchestration/RHEAORU_R01TerminologyValidator.java
+++ src/main/java/org/jembi/rhea/orchestration/RHEAORU_R01TerminologyValidator.java
@@ -7,8 +7,6 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.jembi.rhea.RestfulHttpRequest;
-import org.jembi.rhea.services.CodeValidator;
-import org.jembi.rhea.services.impl.ApelonCodeValidator;
 import org.mule.api.MuleContext;
 import org.mule.api.MuleEventContext;
 import org.mule.api.MuleException;
@@ -39,12 +37,12 @@ public class RHEAORU_R01TerminologyValidator implements Callable {
 		RestfulHttpRequest payload = (RestfulHttpRequest) msg.getPayload();
 		String oru_r01_str = payload.getBody();
 		
-		validateTerminologyInORU_R01(oru_r01_str, new ApelonCodeValidator(client));
+		validateTerminologyInORU_R01(oru_r01_str, client);
 		
 		return msg;
 	}
 	
-	protected void validateTerminologyInORU_R01(String oru_r01_str, CodeValidator validator) throws Exception {
+	protected void validateTerminologyInORU_R01(String oru_r01_str, MuleClient client) throws Exception {
 		Parser p = new GenericParser();
 		ORU_R01 oru_r01 = (ORU_R01) p.parse(oru_r01_str);
 		
@@ -62,11 +60,21 @@ public class RHEAORU_R01TerminologyValidator implements Callable {
 					throw new InvalidTerminologyException("No code or namespace set in ORU_R01, these are required.");
 				}
 				
-				if (!validator.validateCode(namespace, id)) {
+				if (!dispatchToFlow(client, namespace, id)) {
 					throw new UnknownTerminologyException("Unknown term used in ORU_R01, please use only RHEA codes. Unknown code: " + namespace + " " + id);
 				}
 			}
 		}
+	}
+	
+	protected  boolean dispatchToFlow(MuleClient client, String namespace, String code) throws MuleException {
+		Map<String, String> idMap = new HashMap<String, String>();
+		idMap.put("id", code);
+		idMap.put("namespace", namespace);
+		
+		MuleMessage response = client.send("vm://validateterm", idMap, null);
+		String success = response.getInboundProperty("success");
+		return success.equals("true");
 	}
 	
 	
