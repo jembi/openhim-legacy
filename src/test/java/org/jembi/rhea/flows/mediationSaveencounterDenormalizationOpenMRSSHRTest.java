4f39b33bf79ec0dfd692a2e2a76dee1980064b4b
Kari Schoonbee <kari@jembi.org>
2013-05-28 19:52:58 +0200

    Commiting functional tests for SaveEncounter Denormalization OpenSHR flow (Issue 100)

--- src/test/java/org/jembi/rhea/flows/mediationSaveencounterDenormalizationOpenMRSSHRTest.java
+++ src/test/java/org/jembi/rhea/flows/mediationSaveencounterDenormalizationOpenMRSSHRTest.java
@@ -1,26 +1,22 @@
 package org.jembi.rhea.flows;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
-import static com.github.tomakehurst.wiremock.client.WireMock.equalTo;
 import static com.github.tomakehurst.wiremock.client.WireMock.post;
 import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
 import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
 import static org.junit.Assert.*;
 
 import java.io.IOException;
-import java.util.HashMap;
 import java.util.Map;
 
 import org.jembi.Util;
 import org.jembi.rhea.RestfulHttpRequest;
 import org.jembi.rhea.RestfulHttpResponse;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.mule.api.MuleException;
 import org.mule.api.MuleMessage;
-import org.mule.api.transport.PropertyScope;
 import org.mule.module.client.MuleClient;
 import org.mule.tck.junit4.FunctionalTestCase;
 
@@ -28,19 +24,17 @@ import com.github.tomakehurst.wiremock.junit.WireMockRule;
 
 public class mediationSaveencounterDenormalizationOpenMRSSHRTest extends FunctionalTestCase {
 
+	
 	@Rule
 	public WireMockRule wireMockRule = new WireMockRule(8003);
 	
 	
-	private void setupWebserviceStub(int httpStatus, String responseBody) {
-		stubFor(post(urlEqualTo("/openmrs/ws/rest/RHEA/patient/encounters"))
-				.withHeader("Accept", equalTo("application/xml"))
+	private void setupWebserviceStub(int httpStatus) {
+		stubFor(post(urlEqualTo("/openmrs/ws/rest/RHEA/patient/encounters?idType=NID&patientId=1234567890123"))
 		    	.willReturn(aResponse()
-		    		.withStatus(httpStatus)
-		    		.withHeader("Content-Type", "application/xml")
-	                .withBody(responseBody)));
+		    		.withStatus(httpStatus)));
 	}
-	
+		
 	
 	@Override
 	protected String getConfigResources() {
@@ -50,25 +44,43 @@ public class mediationSaveencounterDenormalizationOpenMRSSHRTest extends Functio
 	
 	@Test
 	public void testSaveEncounterValidResponse() throws MuleException, IOException{
-		logger.info("Starting mediationSaveencounterDenormalizationOpenMRSSHRTest");
 		
-		setupWebserviceStub(201, Util.getResourceAsString("SaveEncounterOpenMRSSHRResponse.txt"));
+		setupWebserviceStub(201);
 		
 		MuleClient client = new MuleClient(muleContext);
 		Map<String, Object> properties = null;
 		RestfulHttpRequest req = new RestfulHttpRequest();
-		req.setHttpMethod("POST");
+		req.setHttpMethod(RestfulHttpRequest.HTTP_POST);
 		req.setPath("openmrs/ws/rest/v1/patient/NID-1234567890123/encounters");
 		req.setBody(Util.getResourceAsString("oru_r01.xml"));
 		
 		MuleMessage result = client.send("vm://saveEncounters-De-normailization-OpenMRSSHR", (Object) req, properties);
 		
+
 		Assert.assertTrue(result.getPayload() instanceof RestfulHttpResponse);
 		RestfulHttpResponse resp = (RestfulHttpResponse) result.getPayload();
 	    assertEquals(201, resp.getHttpStatus());
 	    
-	    logger.info("End of mediationSaveencounterDenormalizationOpenMRSSHRTest");
 	}
 	
+	@Test
+	public void testSaveEncounterResourceNotFound() throws MuleException, IOException{
+		
+		setupWebserviceStub(404);
+		
+		MuleClient client = new MuleClient(muleContext);
+		Map<String, Object> properties = null;
+		RestfulHttpRequest req = new RestfulHttpRequest();
+		req.setHttpMethod(RestfulHttpRequest.HTTP_POST);
+		req.setPath("openmrs/ws/rest/v1/patient/NID-1234567890123/encounters");
+		req.setBody(Util.getResourceAsString("oru_r01.xml"));
+		
+		MuleMessage result = client.send("vm://saveEncounters-De-normailization-OpenMRSSHR", (Object) req, properties);
+		
 
+		Assert.assertTrue(result.getPayload() instanceof RestfulHttpResponse);
+		RestfulHttpResponse resp = (RestfulHttpResponse) result.getPayload();
+	    assertEquals(404, resp.getHttpStatus());
+	    
+	}
 }
