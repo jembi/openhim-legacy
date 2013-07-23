2cb25508d6ac71d5af14b8d4d0faec6fe547bbd4
ryan <rg.crichton@gmail.com>
2013-07-23 13:29:19 +0200

    Added config for mock service for the default channel

--- src/test/resources/integration-tests/run-integration-tests.sh
+++ src/test/resources/integration-tests/run-integration-tests.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # This must be set to the path in which your soap ui instance is installed
-SOAPUI_TESTRUNNER="<path-to-soap-ui-install-folder>/bin/testrunner.sh"
+SOAPUI_TESTRUNNER="<path-to-soapui>/soapUI-4.5.2/bin/testrunner.sh"
 
 # Launch standalone wiremock for mocked services
 java -jar wiremock-1.33-standalone.jar --verbose &
@@ -8,7 +8,7 @@ WireMockPID=$!
 sleep 2s
 
 # Launch soap ui integration testing suite
-$SOAPUI_TESTRUNNER -ehttps://localhost:5000 -s"OpenHIM Interface TestSuite" -utest -ptest -r -f. -I OpenHIM-integration-tests.xml
+$SOAPUI_TESTRUNNER -ehttps://localhost:5000 -utest -ptest -r -f. -I OpenHIM-integration-tests.xml
 
 # Kill wiremock dead
 kill -HUP $WireMockPID
