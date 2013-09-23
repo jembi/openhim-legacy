1088e509391224c0e4e9123a3125a6c16945f609
ryan <ryan@jembi.org>
2013-09-23 14:44:39 +0200

    Merged DefaultCahnnelComponent

--- src/main/java/org/jembi/openhim/DefaultChannelComponent.java
+++ src/main/java/org/jembi/openhim/DefaultChannelComponent.java
@@ -60,6 +60,10 @@ public class DefaultChannelComponent implements Callable {
 		msg.setProperty("http.host", mapping.getHost(), PropertyScope.OUTBOUND);
 		msg.setProperty("http.port", mapping.getPort(), PropertyScope.OUTBOUND);
 		
+		if (req.getHttpMethod().equals("PUT") || req.getHttpMethod().equals("POST")) {
+			return req.getBody();
+		}
+		
 		return msg;
 	}
 
