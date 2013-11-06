4fd9ae97dc8ee4fa2ab3241d5bb48c06a8d9d3fe
ryan <ryan@jembi.org>
2013-11-06 09:50:25 +0200

    Altered inferface component to turn off basic auth checking when mutual TLS is enabled

--- src/test/resources/integration-tests/run-mutual-auth-test.sh
+++ src/test/resources/integration-tests/run-mutual-auth-test.sh
@@ -1,10 +1,10 @@
 #/bin/bash
 # The example truststore supplied in src/resources contains the curl-test-keystore cert and key
 # The OpenHIM must be started with the property cert.client.auth=true
-curl -E curl-test-keystore.pem:Jembi#123 -k -u test:test https://localhost:5000/test/protected
+curl -E curl-test-keystore.pem:Jembi#123 -k https://localhost:5000/test/protected
 valid=$?
 
-curl -E curl-test-invalid-keystore.pem:Jembi#123 -k -u test:test https://localhost:5000/test/protected
+curl -E curl-test-invalid-keystore.pem:Jembi#123 -k https://localhost:5000/test/protected
 invalid=$?
 
 if [ $valid = "0" -a $invalid != "0" ]; then
